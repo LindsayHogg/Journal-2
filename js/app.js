94e3c51e5afa955dcee859173a13080666ed4541
LindsayHogg <lindsayhogg@gmail.com>
2015-06-22 15:35:45 -0400

    Practicing Javascript - another journal

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+
+
+
+var myJournal = new Journal();
+myJournal.addEntry("First Entry", "Everything is great!", "Ben", ["Friday", "Boring"]);
+myJournal.addEntry("Second Entry", "What happened? Everything is terrible", "Ben", ["Monday", "Boring"]);
+myJournal.addEntry("Think About It", "Real Eyes Realize Real Lies", "Jaden", ["Deep", "Eyes", "Swag"]);
+myJournal.addEntry("Huh?", "Who gave my journal to Jaden?", "Ben", ["Confused"]);
+
+function writeNumEntriesOnPage(journal)
+{
+	$('#entryCount').html(journal.entries.length + "entries");
+}
+
+function writeEntriesToPage(journal)
+{
+
+	$("#journalContent").html(journal.entries[0].toHTML());
+
+}
+
+writeNumEntriesOnPage(myJournal);
+writeEntriesToPage(myJournal);
+
